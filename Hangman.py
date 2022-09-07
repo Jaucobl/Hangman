fd66f4e1aafdf4691498758743501a566e1c8fdd
Jaucobl <jlaytin@lsoc.org>
2022-09-07 10:59:07 -0500

    T

---
+++ Hangman.py
@@ -0,0 +1,11 @@
+#Create the gallow that the man would be hanged upon
+#First we would ask the user for a word that is going to be guessed
+#Create the variable that the word would be stored in
+#Clear the word off of the screen
+#Take the length of the word and create a number of underscores equal to it
+#If there are spaces in users input, keep them as spaces
+#Then we would ask another user for a letter guess
+#If the letter is in the word, put the letter in its original position
+#Repeat asking the user for a letter until the user runs out of guesses or the word is guessed
+#If they fail then after the hangman dies, display a game over screen and then offer an option to restart
+#If they succeed, then display a you won screen and then offer an option to restart
