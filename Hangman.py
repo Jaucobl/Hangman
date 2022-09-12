c4ca864e0f49bfa24ff5dadf7c4adea34e52dbef
lreshefs <lreshefs@lsoc.org>
2022-09-12 14:46:51 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -7,10 +7,12 @@ print(|)
 print(|)
 print(-----------)
 #First we would ask the user for a word that is going to be guessed
-word = input("enter a word or sentence without punctuation")
+words = input("enter a word or sentence without punctuation")
 #Create the variable that the word would be stored in
 #Clear the word off of the screen
 #Take the length of the word and create a number of underscores equal to it
+length = len(words)
+
 #If there are spaces in users input, keep them as spaces
 #Then we would ask another user for a letter guess
 #If the letter is in the word, put the letter in its original position
