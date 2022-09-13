573bc1b7c0eb01b6cf6706b21f9cab55d1da6007
Jaucobl <jlaytin@lsoc.org>
2022-09-13 13:39:41 -0500

    T

--- Hangman.py
+++ Hangman.py
@@ -1,16 +1,23 @@
 #Create the gallow that the man would be hanged upon
-print(----------)
-print(|        |)
-print(|        |)
-print(|)
-print(|)
-print(|)
-print(-----------)
+print("----------")
+print("|        |")
+print("|        |")
+print("|")
+print("|")
+print("|")
+print("-----------")
 #First we would ask the user for a word that is going to be guessed
-word = input("enter a word or sentence without punctuation")
+words = input("enter a word or sentence without punctuation")
 #Create the variable that the word would be stored in
-os.system('cls')
 #Clear the word off of the screen
+new = []
+for each in list(words):
+    if each != " ":
+        new.append("_")
+    else:
+        new.append(" ")
+
+print(new)
 #Take the length of the word and create a number of underscores equal to it
 #If there are spaces in users input, keep them as spaces
 #Then we would ask another user for a letter guess
